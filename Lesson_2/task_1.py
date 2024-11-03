98524a977555527ded63cfd1384100fc3781aac9
ValueInf <petrossstepanio@gmail.com>
2024-11-03 04:04:18 +0000

    task_1

---
+++ Lesson_2/task_1.py
@@ -0,0 +1,7 @@
+profile_number = 1
+wallet_address = "x0Fn7654n3543445646Scgsxfgd7564"
+wallet_balance = 0.240
+ethereum_price = 3750
+balance_wallet_in_USD = wallet_balance * ethereum_price
+print(f"запущен профиль: {profile_number}, кошелек: {wallet_address}, баланс: {wallet_balance} ETH, "
+                        f"баланс {profile_number}: {balance_wallet_in_USD} USD")
